3073201f9f62c020dfdcae47ece0d22843885754
kanhaiyakumawat <kk.nitrkl@gmail.com>
2013-02-04 15:58:37 +0530

    Update towerofhanoi.cpp

--- towerofhanoi.cpp
+++ towerofhanoi.cpp
@@ -1,30 +1,25 @@
-#include<iostream>
- 
+#include <iostream>
+
 using namespace std;
-void move(int n,char *s,char *i,char *d)
-// s stands for source tower
-// d stands for destination tower
-// i stands for intermediate tower
+
+void hanoi(char * source, char * destination, char * intermediate, int disks)
 {
- if(n>0)
- {
-  move(n-1,s,d,i);
-  // move n-1 disks from source to intermediate tower
-  cout<<"disk "<<n<<" is moved from "<<s<<" to "<<d<<endl;
-  // move the disk from to source to destination
-  move(n-1,i,s,d);
-  // move n-1 disks from intermediate to destination
- }
+if(disks > 0)
+{
+ //move n - 1 discks to intermediate VIA destination
+hanoi(source, intermediate, destination, disks - 1);
+//now move the remaining 1 disk to destination from source
+cout<<"disk "<<disks<<" moved from "<<source<<" to destination "<<destination<<endl;
+//now move rest of the n - 1 disks from intermediate to destination VIA source
+hanoi(intermediate, destination, source, disks - 1);
 }
- 
+}
 int main()
 {
- cout<<"\n**********************************************************\n";
- cout<<"This C++ program is to solve the towers of hanoi problem";
- cout<<"\n**********************************************************\n";
- cout<<"Enter the no. of disks ";
- int n;
- cin>>n;
- move(n,"source tower","intermediate tower","destination tower");
- return 0;
+int disks = 0;
+cout<<"enter no of disks\n";
+cin>>disks;
+
+hanoi("A", "C", "B", disks);
+return 0;
 }
