8e5269e1766508f60687316da231e996620a0ae1
root <root@V-MAA-00438849.(none)>
2014-05-03 06:38:20 -0700

    fixed bug for dialpad_permutation program

--- cplusplusprograms/dialpad_permutation.cpp
+++ cplusplusprograms/dialpad_permutation.cpp
@@ -4,36 +4,44 @@ using namespace std;
 
 class Key
 {
-	public:
-		void set(char* str, int len)
-		{
-			this->str = new char[len];
-			int i = 0;
-			while(i < len)
-			{
-				this->str[i] = str[i];
-				i++;
-			}
-			this->len = len;
-			current = 0;
-		}
-		char get_next()
-		{
-			if(current < len)
-			{
-			//	cout<<"current key is "<<str<<" returning : "<<str[current]<<endl;
-				return str[current++];
-			}
-			throw '\0';
-		}
-		void reset()
-		{
-			current = 0;
-		}
-	private:
-		int len;
-		char *str;
-		int current;
+public:
+  void set (char *str, int len)
+  {
+    this->str = new char[len];
+    int i = 0;
+    while (i < len)
+      {
+	this->str[i] = str[i];
+	i++;
+      }
+    this->len = len;
+    current = 0;
+  }
+
+  char get_next ()
+  {
+    if (current < len)
+      {
+	//      cout<<"current key is "<<str<<" returning : "<<str[current]<<endl;
+	return str[current++];
+      }
+    throw '\0';
+  }
+
+  void reset ()
+  {
+    current = 0;
+  }
+
+  int get_length ()
+  {
+    return len;
+  }
+
+private:
+  int len;
+  char *str;
+  int current;
 };
 
 class DialPad
@@ -42,8 +50,24 @@ public:
   void permute (char *num, int len)
   {
     set_keys ();
+    int curr_ind = 0;
+    int i = 0;
+    int good_len = len;
+    char *good_num = new char[len];
+    while (curr_ind < len)
+      {
+	if (keys[num[curr_ind] - '0'].get_length () <= 1)
+	  {
+	    good_len--;
+	  }
+	else
+	  {
+	    good_num[i++] = num[curr_ind];
+	  }
+	curr_ind++;
+      }
     char *curr = new char[len];
-    permute (num, curr, len, 0);
+    permute (good_num, curr, good_len, 0);
   }
 private:
   Key keys[10];
@@ -81,7 +105,7 @@ int
 main ()
 {
   DialPad dp;
-  char *num = "234";
-  dp.permute (num, 3);
+  char *num = "01234567";
+  dp.permute (num, 8);
   return 0;
 }
